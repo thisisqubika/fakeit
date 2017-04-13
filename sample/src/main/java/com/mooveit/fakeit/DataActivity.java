23d433b50b2b6bc4b12399f7a8d4aa87649b7c86
Juan Pereira <juanmanuel.pereira1@gmail.com>
2017-04-13 13:54:34 -0300

    Add language selector on sample app

--- sample/src/main/java/com/mooveit/fakeit/DataActivity.java
+++ sample/src/main/java/com/mooveit/fakeit/DataActivity.java
@@ -3,10 +3,15 @@ package com.mooveit.fakeit;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.mooveit.fakeit.utils.Constants;
 import com.mooveit.fakeit.utils.FragmentUtils;
+import com.mooveit.library.Fakeit;
 
 public class DataActivity extends AppCompatActivity {
 
@@ -39,12 +44,91 @@ public class DataActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.locales, menu);
+
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case (R.id.english):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.EN.locale());
+                break;
+            case (R.id.russian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.RU.locale());
+                break;
+            case (R.id.spanish):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.ES.locale());
+                break;
+            case (R.id.german):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.DE.locale());
+                break;
+            case (R.id.catalan):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.CA.locale());
+                break;
+            case (R.id.persian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.FA.locale());
+                break;
+            case (R.id.finnish):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.FI.locale());
+                break;
+            case (R.id.french):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.FR.locale());
+                break;
+            case (R.id.hebrew):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.HE.locale());
+                break;
+            case (R.id.indonesian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.ID.locale());
+                break;
+            case (R.id.italian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.IT.locale());
+                break;
+            case (R.id.japanese):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.JA.locale());
+                break;
+            case (R.id.korean):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.KO.locale());
+                break;
+            case (R.id.norwegian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.NB.locale());
+                break;
+            case (R.id.dutch):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.NL.locale());
+                break;
+            case (R.id.polish):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.PL.locale());
+                break;
+            case (R.id.portuguese):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.PT.locale());
+                break;
+            case (R.id.slovak):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.SK.locale());
+                break;
+            case (R.id.swedish):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.SV.locale());
+                break;
+            case (R.id.turkish):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.TR.locale());
+                break;
+            case (R.id.ukrainian):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.UK.locale());
+                break;
+            case (R.id.vietnamese):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.VI.locale());
+                break;
+            case (R.id.chinese):
+                Fakeit.resetLocale(this, Constants.FakeitLocale.ZH.locale());
+                break;
             case android.R.id.home:
                 super.onBackPressed();
-                return true;
         }
-        return super.onOptionsItemSelected(item);
+
+        recreate();
+
+        return true;
     }
 }
